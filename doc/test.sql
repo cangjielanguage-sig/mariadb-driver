08790341615a76c318028e9a7b7fa7d87dc87804
yesokim <yesokim@163.com>
2025-05-08 16:44:28 +0800

    update

--- doc/test.sql
+++ doc/test.sql
@@ -1,13 +1,13 @@
-#创建测试数据库
-CREATE DATABASE driver_test;
-USE driver_test;
-
 #创建测试用户
 CREATE USER 'native_user'@'%' IDENTIFIED WITH mysql_native_password BY 'native_user';
 CREATE USER 'sha256_user'@'%' IDENTIFIED WITH sha256_password BY 'sha256_user';
 CREATE USER 'caching_user'@'%' IDENTIFIED WITH caching_sha2_password BY 'caching_user';
 CREATE USER 'test_user'@'%' IDENTIFIED BY 'test_user';
 GRANT ALL PRIVILEGES ON driver_test.* TO 'test_user'@'%';
+
+#创建测试数据库
+CREATE DATABASE driver_test;
+USE driver_test;
 
 #创建测试表
 CREATE TABLE `full_test`  (
@@ -45,4 +45,4 @@ CREATE TABLE `full_test`  (
   `set_col` set('value1','value2','value3','value4','value5'),
   `enum_col` enum('value1','value2','value3','value4','value5') ,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+);
