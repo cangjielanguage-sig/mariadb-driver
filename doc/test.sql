4846b204e645ac7dc6db7675bcc413483f83aeb0
yesokim <yesokim@163.com>
2025-07-29 14:16:49 +0800

    add mariadb test

--- doc/test.sql
+++ doc/test.sql
@@ -9,44 +9,6 @@ GRANT ALL PRIVILEGES ON driver_test.* TO 'test_user'@'%';
 CREATE DATABASE driver_test;
 USE driver_test;
 
--- #创建测试表
--- CREATE TABLE `full_test`  (
---   `id` bigint NOT NULL AUTO_INCREMENT,
---   `bool_col` tinyint(1) ,
---   `tinyint_col` tinyint ,
---   `utinyint_col` tinyint UNSIGNED ,
---   `smallint_col` smallint,
---   `usmallint_col` smallint UNSIGNED ,
---   `mediumint_col` mediumint ,
---   `umediumint_col` mediumint UNSIGNED,
---   `int_col` int ,
---   `uint_col` int UNSIGNED ,
---   `bigint_col` bigint ,
---   `ubigint_col` bigint UNSIGNED,
---   `float_col` float ,
---   `double_col` double ,
---   `decimal_col` decimal(65, 30),
---   `date_col` date ,
---   `time_col` time ,
---   `datetime_col` datetime ,
---   `timestamp_col` timestamp,
---   `year_col` year ,
---   `char_col` char(255) ,
---   `varchar_col` varchar(255) ,
---   `tinytext_col` tinytext ,
---   `text_col` text,
---   `mediumtext_col` mediumtext,
---   `longtext_col` longtext,
---   `tinyblob_col` tinyblob ,
---   `blob_col` blob ,
---   `mediumblob_col` mediumblob ,
---   `longblob_col` longblob ,
---   `json_col` json ,
---   `set_col` set('value1','value2','value3','value4','value5'),
---   `enum_col` enum('value1','value2','value3','value4','value5') ,
---   PRIMARY KEY (`id`)
--- );
-
 #数字类型测试表
 CREATE TABLE numeric_types (
   `bool_col` tinyint(1) ,             -- bool_col (TINYINT(1), 0/1)
